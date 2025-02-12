6be6d5c50a43f911791e5124c556b4617ca3a4da
fatah4561 <fatahathariq6@gmail.com>
2025-02-12 15:16:28 +0700

    fix: get profile convert id to int
    also fix auth alg to match PS256

--- services/user/user.ts
+++ services/user/user.ts
@@ -3,22 +3,28 @@ import { UserEntity, Users } from "./db"
 
 export const getSingleUser = api(
     { method: "POST", path: "/user:username" }, // TODO refactor change to get so it is REST API compliant
-    async ({username, fields}: {username: string, fields?: Array<string>}): Promise<{user: UserEntity}> => {
+    async ({ username, fields }: { username: string, fields?: Array<string> }): Promise<{ user: UserEntity }> => {
         const query = Users().
-        where("username", username)
-        
+            where("username", username)
+
         if (fields) {
             query.column(fields)
         }
-        
+
         const user = await query.
-        select<UserEntity>().
-        first()
+            select<UserEntity>().
+            first()
+        if (user) {
+            user.id = Number(user.id);
+        }
 
         if (!user) {
             throw new APIError(ErrCode.NotFound, "User not found")
+        } else {
+            // potgresql knex return bigint as string so this it to number
+            user.id = Number(user.id) 
         }
 
-        return {user: user?? null}
+        return { user: user ?? null }
     }
 )
