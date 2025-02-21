fcefc793f80b81d373bf6306d5acc862d655c1cd
fatah4561 <fatahathariq6@gmail.com>
2025-02-21 17:27:43 +0700

    chore: add JS doc in error helper

--- helpers/error.ts
+++ helpers/error.ts
@@ -1,6 +1,12 @@
 import { APIError, ErrCode } from "encore.dev/api";
 import { ErrorResponse } from "../graphql/__generated__/resolvers-types";
 
+/**
+ * Parse error object into ErrorResponse object.
+ * If error is not an instance of APIError, it will use ErrCode.Internal as code and String(err) as message
+ * @param err - error object
+ * @returns ErrorResponse object
+ */
 export const parseError = (err: any): ErrorResponse => {
     const error = err as APIError
     return {
